0ab28941174a046adc87a4c5a1013c70553e3321
Markus02020 <stmd2008@gmail.com>
2026-03-23 10:27:37 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5.Variant00
+{
+    public class Task1
+    {
+        public struct Number : INumber
+        {
+            private double _real;
+            
+            public double Real => _real;
+            public double Abs => Math.Abs(_real);
+            public int Sign => Math.Sign(_real);
+            public Number(double real)
+            {
+                _real = real;
+                
+            }
+            public void Sum(INumber other)
+            {
+                if (other == null)
+                {
+                    return;
+                }
+                _real += other.Real;
+            }
+            public void Sub(INumber other)
+            {
+                if (other == null)
+                {
+                    return;
+                }
+                _real -= other.Real; 
+            }
+            public void Mul(INumber other)
+            {
+                if (other == null)
+                {
+                    return;
+                }
+                _real *= other.Real;
+            }
+            public void Div(INumber other)
+            {
+                if (other.Real == 0)
+                {
+                    return;
+                }
+                _real /= other.Real;
+            }
+            public void Neg()
+            {
+                if (_real == 0)
+                {
+                    return;
+                }
+                _real *= -1;
+            }
+        }
+
+    }
+}
